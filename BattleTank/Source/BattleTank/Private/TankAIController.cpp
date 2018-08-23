41dec47abd6b9444cf8867e7873ceb08f9cc7e8b
KELBYS-SB2\kelby <kelbyvp@gmail.com>
2018-08-23 01:04:50 -0700

    Lecture 131 -- Get the Player Controller with C++

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -2,6 +2,7 @@
 
 #include "TankAIController.h"
 #include "BattleTank.h"
+#include "GameFramework/Actor.h"
 
 
 
@@ -9,21 +10,26 @@ void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto *ControlledTank = GetControlledTank();
-	if (!ControlledTank)
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("AIController not possessing a tank"));
+		UE_LOG(LogTemp, Warning, TEXT("AIController can't find player tank"));
 	}
 	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("AI Controller Controlling: %s"), *(ControlledTank->GetName()));
-	}
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller found player: %s"), *PlayerTank->GetName());
 }
+
+
+
 
 ATank* ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
 
-
-
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
+}
