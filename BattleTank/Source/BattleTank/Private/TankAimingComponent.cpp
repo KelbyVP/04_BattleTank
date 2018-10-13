8612d7ad4341d0d61c032eb9d385dca28e52f612
KELBYS-SB2\kelby <kelbyvp@gmail.com>
2018-10-12 20:57:23 -0700

    Lecture 4.143 - Using FRotators

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -5,40 +5,29 @@
 #include "Components/StaticMeshComponent.h"
 #include "Components/SceneComponent.h"
 
-
-// Sets default values for this component's properties
 UTankAimingComponent::UTankAimingComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
+
 
 void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent * BarrelToSet)
 {
 	Barrel = BarrelToSet;
 }
 
-// Called when the game starts
-void UTankAimingComponent::BeginPlay()
+void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 {
-	Super::BeginPlay();
+		// figure out how far off the barrel is from where we want it to be (current barrel rotation vs AimDirection)
+		auto BarrelRotator = Barrel->GetForwardVector().Rotation();
+		auto AimAsRotator = AimDirection.Rotation();
+		UE_LOG(LogTemp, Warning, TEXT("Aim Rotation:  %s; Barrel Rotation: %s"), *AimAsRotator.ToString(), *BarrelRotator.ToString());
+		auto DeltaRotation = AimAsRotator - BarrelRotator;
 
-	// ...
-	
+		// rotate the barrel the right amount this frame
+		// given the max elevation speed and frame time
 }
 
-
-// Called every frame
-void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-
-}
 
 void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 {
@@ -47,23 +36,24 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
 
 	// Calculate the TossVelocity
-	if (UGameplayStatics::SuggestProjectileVelocity(
+	bool bHaveAimSolution = (UGameplayStatics::SuggestProjectileVelocity(
 		this,
 		OUT TossVelocity,
 		StartLocation,
 		HitLocation,
 		LaunchSpeed,
-		false,
-		0,
-		0,
-		ESuggestProjVelocityTraceOption::DoNotTrace))
+		ESuggestProjVelocityTraceOption::DoNotTrace));
+
+	if (bHaveAimSolution)
 	{
 		auto AimDirection = TossVelocity.GetSafeNormal();
-		auto TankName = GetOwner()->GetName();
-		UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *TankName, *AimDirection.ToString())
+		MoveBarrelTowards(AimDirection);
 	}
-	else return;
+	else
+	{
+		return;
 
+	}
 }
 
 
--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Kismet/GameplayStatics.h"
 #include "Components/ActorComponent.h"
+#include <vector>
 #include "TankAimingComponent.generated.h"
 
 
@@ -20,15 +21,12 @@ public:
 
 	void SetBarrelReference(UStaticMeshComponent * BarrelToSet);
 
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	// TODO add SetTurretReference
 
 	void AimAt(FVector WorldSpaceAim, float LaunchSpeed);
 
 private:
 	UStaticMeshComponent * Barrel = nullptr;
+	void MoveBarrelTowards(FVector AimDirection);
 
 };
