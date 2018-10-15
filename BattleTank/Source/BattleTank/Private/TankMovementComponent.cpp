4ffdc71e5a1e6681851db1699da7c76150a1bdb0
KELBYS-SB2\kelby <kelbyvp@gmail.com>
2018-10-14 17:14:14 -0700

    Lec 4:166 - A Better Component Architecture

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,7 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
 
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveFoward(float Throw)
 {
@@ -9,5 +17,12 @@ void UTankMovementComponent::IntendMoveFoward(float Throw)
 	//auto Time = GetWorld->GetTimeSeconds();
 	auto Name = GetOwner()->GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s intends move forward at throw = %f"), *Name, Throw);
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+
+	// TODO preventg double-speed due to control using both stick and shoulder buttons
+	
 }
+
 
