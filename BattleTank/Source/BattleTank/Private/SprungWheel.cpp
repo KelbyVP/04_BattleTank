14b2d766ddfcc4dfbf7e1ab5ee647b9082eb68cd
KELBYS-SB2\kelby <kelbyvp@gmail.com>
2018-11-01 22:26:04 -0700

    Lec 217 - Attachment, Actors and Components

--- BattleTank/Source/BattleTank/Private/SprungWheel.cpp
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -1,6 +1,7 @@
 // Copyright KVP
 
 #include "SprungWheel.h"
+#include "TankTrack.h"
 
 
 // Sets default values
@@ -9,15 +10,16 @@ ASprungWheel::ASprungWheel()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	MassWheelConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Mass Wheel Constraint"));
+	SetRootComponent(MassWheelConstraint);
+
 	Mass = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mass"));
-	SetRootComponent(Mass);
+	Mass->SetupAttachment(MassWheelConstraint);
 
 	Wheel = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel"));
-	Wheel->SetupAttachment(Mass);
+	Wheel->SetupAttachment(MassWheelConstraint);
 
-	MassWheelConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Mass Wheel Constraint"));
-	MassWheelConstraint->SetupAttachment(Mass);
-	MassWheelConstraint->bAutoActivate = true;
+
 
 }
 
@@ -25,7 +27,14 @@ ASprungWheel::ASprungWheel()
 void ASprungWheel::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	if (!GetAttachParentActor())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Null"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("NotNull"))
+	}
 }
 
 // Called every frame
